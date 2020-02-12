3ff224dbb7d46f2402773cdff0ceebed866460cf
André Matthies <matthiez@gmail.com>
2020-02-12 09:42:00 +0100

    added signature position

--- src/util/General.js
+++ src/util/General.js
@@ -9,24 +9,15 @@ export class General {
         }
     }
 
-    static async notify(message, title = 'SMS Status', type = 'basic', id = '') {
-        return browser.notifications.create(id, {
-            type,
-            title,
-            message: 'string' === typeof message
-                ? message : message.message
-                    ? message.message : JSON.stringify(message),
+    static async notify(message, title = 'SMS Status', type = 'basic') {
+        return browser.runtime.sendMessage({
+            action: 'NOTIFY', notification: {
+                type,
+                title,
+                message: 'string' === typeof message
+                    ? message : message.message
+                        ? message.message : JSON.stringify(message),
+            }
         });
-    }
-
-    static async prompt(msg, value = null) {
-        if ('moz-extension:' === location.protocol) {
-            const input = await browser.tabs.executeScript({code: `window.prompt('${msg}')`});
-            return input.length ? input.shift() : null;
-
-        } else {
-            return window.prompt(msg);
-        }
-
     }
 }
